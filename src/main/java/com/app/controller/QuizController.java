6a7dc0efb80c4166384cdef266f93ecb6a53cdd9
psm0419 <sm8804@naver.com>
2025-01-10 15:17:49 +0900

    request 파라미터 전달 연습

--- src/main/java/com/app/controller/QuizController.java
+++ src/main/java/com/app/controller/QuizController.java
@@ -1,26 +1,38 @@
 package com.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QuizController {
 
-	@GetMapping("/quiz/main")
-	public String main() {
-		
-		return "quiz/main";
+	
+	@GetMapping("/quiz/req1")
+	public String req1 () {
+		System.out.println("/quiz/req1 GET 요청");
+		return "quiz/req1";
+	}
+	@PostMapping("/quiz/req1")
+	public String req1Post () {
+		System.out.println("/quiz/req1 POST 요청");
+		return "quiz/req1";
 	}
 	
-	@GetMapping("/quiz/product/mouse")
-	public String mouse() {
+	@RequestMapping("/quiz/req2")
+	public String req2(HttpServletRequest request) {
 		
-		return "quiz/product/mouse";
-	}
-	
-	@GetMapping("/quiz//product/keyboard")
-	public String keyboard() {
+		System.out.println(request.getParameter("data1"));
+		System.out.println(request.getParameter("data2"));
 		
-		return "quiz//product/keyboard";
+		System.out.println("/quiz/req2 요청");
+		
+		request.setAttribute("data1", request.getParameter("data1"));
+		request.setAttribute("data2", request.getParameter("data2"));
+		
+		return "quiz/req2";
 	}
 }
