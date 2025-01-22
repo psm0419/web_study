e5a697370c00ef039336d01382ec05a597312445
psm0419 <sm8804@naver.com>
2025-01-22 13:51:04 +0900

    공통코드 상수화 인터페이스에 정리

---
+++ src/main/java/com/app/common/CommonCode.java
@@ -0,0 +1,14 @@
+package com.app.common;
+
+public interface CommonCode {
+	//전체 서비스 내에서 사용하는 모든 상수형 코드들을 모아놓은 파일
+	
+	//USER
+	public String USER_USERTYPE_CUSTOMER = "CUS";
+	public String USER_USERTYPE_ADMIN = "ADM";
+	
+	//ROOM
+	public String ROOM_VIEWTYPE_OCEAN = "OCN";
+	public String ROOM_VIEWTYPE_CITY = "CTY";
+	public String ROOM_VIEWTYPE_MOUNTAIN = "MOT";
+}
