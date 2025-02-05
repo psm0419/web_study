0f996795363a3b0124c2e97d3b81abc59e2230fe
psm0419 <sm8804@naver.com>
2025-02-05 12:04:06 +0900

    스케쥴링 처리

---
+++ src/main/java/com/app/scheduler/MyScheduler.java
@@ -0,0 +1,19 @@
+package com.app.scheduler;
+
+import java.time.LocalDate;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class MyScheduler {
+	
+	@Scheduled(cron = "0/5 * * * * *")
+	public void schedule() {
+		System.out.println("schedule"+ LocalDate.now());
+		
+		//api 요청
+		//외부 공공데이터 API 요청 -> Service 데이터 획득
+		
+		//api 요청 데이터 DB 저장
+		//apidata 테이블 -> insert 저장(apidataDAO)
+	}
+}
